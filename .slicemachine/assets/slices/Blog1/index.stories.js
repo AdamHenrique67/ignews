02d0fde2a1312181b0ca8aa4253d5047fbfa7f2e
Adam1a2 <adamhenrique67@gmail.com>
2022-05-30 17:49:58 -0300

    add post using prismic.io

---
+++ .slicemachine/assets/slices/Blog1/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Blog1';
+
+export default {
+  title: 'slices/Blog1'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"blog1","items":[],"primary":{"title":[{"type":"heading1","text":"Transition out-of-the-box partnerships","spans":[]}],"description":[{"type":"paragraph","text":"Ea reprehenderit consectetur incididunt magna ipsum nisi reprehenderit mollit. Esse velit aliqua dolor commodo adipisicing.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
